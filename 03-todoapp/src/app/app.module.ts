5413c7b955238175b0a014f5f5f6c9ce6bdda175
Jesús Rodríguez <jesus.jarp@gmail.com>
2018-10-22 13:31:36 +0200

    Adding some logic to the todoapp

--- 03-todoapp/src/app/app.module.ts
+++ 03-todoapp/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { todoReducer } from './todo/todo.reducer';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodosListComponent } from './todo/todos-list/todos-list.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodosListComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({ todos: todoReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 03-todoapp/src/app/todo/todo.reducer.ts
@@ -0,0 +1,20 @@
+import * as fromTodo from './todo.actions';
+import { Todo } from './todo.model';
+
+
+const todo1 = new Todo('Vencer a Thanos');
+const todo2 = new Todo('Salvar el mundo');
+
+const estadoInicial: Todo[] = [todo1, todo2];
+
+export function todoReducer(state = estadoInicial,
+                            action: fromTodo.Acciones): Todo[] {
+  switch (action.type ) {
+    case fromTodo.AGREGAR_TODO:
+      const todo = new Todo(action.texto);
+      return [ ...state, todo ];
+    default:
+      return state;
+  }
+
+}
